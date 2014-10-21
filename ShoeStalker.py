dee5b3da9eab0329af12a72a1b325d41183c3f22
Claire Diehl <claire.diehl@students.olin.edu>
2014-10-21 15:24:20 -0400

    Added basic structure.

--- ShoeStalker.py
+++ ShoeStalker.py
@@ -6,8 +6,21 @@ class ShoeStalker:
 	def __init__(self):
 
 	def detect(self):
+		#find the shoe
 
-	def track(self):
+		#compare keypoints/color to shoe database
+		#return location of shoes
 
-if __name__ = '__main__'
+	def stalk(self):
+		#move towards the shoes
+
+	def lostshoe():
+		#refind a lost shoe, turn towards location of last view
+
+if __name__ == '__main__':
+	
+	capture = cv2.VideoCapture(0)
+	ret, frame = capture.read()
+	cv2.namedWindow('image')
+	cv2.imshow("image",frame)
 
