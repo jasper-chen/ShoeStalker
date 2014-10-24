2aae0bc44162b20c78b9d7f38d3c60e861533a27
Claire Diehl <claire.diehl@students.olin.edu>
2014-10-24 14:50:36 -0400

    Committing so Amanda has more up to date code. Nothing works yet. Urg.

--- ShoeStalker.py
+++ ShoeStalker.py
@@ -9,8 +9,20 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 class ShoeStalker:
+	SELECTING_NEW_IMG = 0
 
-	def __init__(self):
+	def __init__(self,descriptor):
+		self.detector = cv2.FeatureDetector_create(descriptor)
+		self.extractor = cv2.DescriptorExtractor_create(descriptor)
+		self.matcher = cv2.BFMatcher()
+		self.new_img = None
+		self.new_region = None
+		self.last_detection = None
+
+		self.corner_threshold = 0.0
+		self.ratio_threshold = 1.0
+
+		self.state = ShoeStalker.SELECTING_NEW_IMG
 
 	def capture(self):
 		#take picture of shoe 
@@ -27,6 +39,19 @@ class ShoeStalker:
 		cap.release()
 		#cv2.destroyAllWindows() #perhaps we don't want this? Don't want this till the end
 
+	def get_keypoints(self):
+		#makes new image black and white
+		new_imgbw = cv2.cvtColor(self.new_img,cv2.COLOR_BGR2GREY)
+		keyp = self.detector.detect(new_imgbw)
+		keyp = [pt
+			  for pt in kp if (pt.response > self.corner_threshold and
+							   self.query_region[0] <= pt.pt[0] < self.query_region[2] and
+							   self.query_region[1] <= pt.pt[1] < self.query_region[3])]
+		dc, des = self.extractor.compute(new_imgbw,keyp)
+		#remap so relative to region
+		for point in keyp:
+			point.point = (point.point[0] - self.new_region[0],point.point[1] - self.new_region[1])
+
 	def detect(self):
 		#find the shoe
 
@@ -41,7 +66,8 @@ class ShoeStalker:
 		#refind a lost shoe, turn towards location of last view
 
 if __name__ == '__main__':
-	
+	tracker = ShoeStalker('SIFT')
+
 	capture = cv2.VideoCapture(0)
 	ret, frame = capture.read()
 	cv2.namedWindow('image')
